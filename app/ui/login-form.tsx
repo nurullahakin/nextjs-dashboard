b11f016ac3e3cc99a7098252af150121abf5620f
Nurullah Akın <nurullah.at.work@outlook.com>
2026-08-12 17:28:20 +0300

    (ui) app\ui\login-form.tsx: remove redundant space-y-3

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -17,7 +17,7 @@ export default function LoginForm() {
   const callbackUrl = searchParams.get('callbackUrl') || '/dashboard';
   const [errorMessage, formAction, isPending] = useActionState(authenticate, undefined);
   return (
-    <form action={formAction} className="space-y-3">
+    <form action={formAction}>
       <div className="flex-1 rounded-lg bg-gray-50 px-6 py-6">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
           Please log in to continue.
